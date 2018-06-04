01fad3e5bc1efe40323e93c0cc131d04f89113ee
msoh <ohms@kr.ibm.com>
2018-06-04 13:01:23 +0900

    desc id fixed

--- src/main/java/com/doubles/qna/web/HomeController.java
+++ src/main/java/com/doubles/qna/web/HomeController.java
@@ -2,6 +2,8 @@ package com.doubles.qna.web;
 
 import com.doubles.qna.domain.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +17,9 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("questions", questionRepository.findAll());
+    	
+    	Sort sort = new Sort(Direction.DESC, "id");
+        model.addAttribute("questions", questionRepository.findAll(sort)); 
         return "index";
     }
 
